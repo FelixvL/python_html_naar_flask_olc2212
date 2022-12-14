e4c64d11b8b7d99257001408035d33c722e4492a
FelixvL <digitaleservice@gmail.com>
2022-12-14 13:50:09 +0100

    cors filter toegevoegd

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 import eigenbestand
+from flask_cors import CORS, cross_origin
+
+
 
 
 app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/uitdedb")
+@cross_origin()
 def uitdedb():
     return eigenbestand.ganaardedb()
